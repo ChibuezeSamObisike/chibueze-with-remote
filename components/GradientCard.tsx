799e3c9efd63fa22966642a3565633753973a592
ChibuezeSamObisike <samobisike@gmail.com>
2025-01-10 15:07:32 +0100

    add: error and fixes

--- components/GradientCard.tsx
+++ components/GradientCard.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Box, Text } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+
+const MotionBox = motion(Box);
 
 const GradientCard = () => {
   return (
-    <Box
+    <MotionBox
       width='full'
       py={20}
       bg='radial-gradient(
@@ -21,15 +24,34 @@ const GradientCard = () => {
       fontWeight='bold'
       fontSize='xl'
       position='relative'
+      initial={{ scale: 0.8, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      transition={{ duration: 0.8, ease: 'easeInOut' }}
+      whileHover={{ scale: 1.05 }}
     >
       <Box position='absolute' bottom={5} left={10}>
-        <Text textStyle='h2'>Frontend Mentor</Text>
-        <Text textStyle='body2' opacity='75%'>
+        <MotionText
+          textStyle='h2'
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5, duration: 0.8 }}
+        >
+          Frontend Mentor
+        </MotionText>
+        <MotionText
+          textStyle='body2'
+          opacity='75%'
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.7, duration: 0.8 }}
+        >
           Feedback Board
-        </Text>
+        </MotionText>
       </Box>
-    </Box>
+    </MotionBox>
   );
 };
+
+const MotionText = motion(Text);
 
 export default GradientCard;
